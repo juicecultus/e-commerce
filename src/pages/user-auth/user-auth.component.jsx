92798e099fb2cb93a895af27f3c8b72c2ad0bea1
juicecultus <juicecultus@gmail.com>
2020-04-13 19:13:36 +0100

    add user auth component, sign-in form and reusable custom button

---
+++ src/pages/user-auth/user-auth.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './user-auth.styles.scss';
+
+const UserAuth = () => (
+  <div className='user-auth'>
+    <SignIn />
+  </div>
+);
+
+export default UserAuth;
